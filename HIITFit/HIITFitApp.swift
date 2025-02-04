7f3cd35c19551b36fc2bc8bc884a0c78b1954c03
samlee03 <sam.lee03@myhunter.cuny.edu>
2025-02-04 14:08:19 -0500

    Initial Commit

---
+++ HIITFit/HIITFitApp.swift
@@ -0,0 +1,17 @@
+//
+//  HIITFitApp.swift
+//  HIITFit
+//
+//  Created by Jenn Lee on 2/4/25.
+//
+
+import SwiftUI
+
+@main
+struct HIITFitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
